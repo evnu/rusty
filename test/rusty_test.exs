a00056527b0ce322c6a1c5f07ebbc7a417cbacfc
Magnus Ottenklinger <evnu@posteo.de>
2017-12-15 21:50:24 +0100

    Increase number of tests and document some failed inputs
    Some inputs failed with "Bus Error" (signal 7). When the terms are used
    on their own, this does not happen.

--- test/rusty_test.exs
+++ test/rusty_test.exs
@@ -2,10 +2,12 @@ defmodule RustyTest do
   use ExUnit.Case
   use PropCheck
 
+  @tag timeout: 60_000
   property "echo term" do
     numtests(
-      1000,
+      1000_000,
       forall t <- term() do
+        File.write!("/tmp/data", :erlang.term_to_binary(t))
         result = Rusty.echo_term(t)
         when_fail(
           t == result,
@@ -13,5 +15,17 @@ defmodule RustyTest do
         )
       end
       )
+  end
+
+  test "failed input 1" do
+    # found with 'echo term' property, but does not fail on its own
+    r = <<231, 0::size(2)>>
+    assert r == Rusty.echo_term(r)
+  end
+
+  test "failed input 2" do
+    # found with 'echo term' property, but does not fail on its own
+    r = :""
+    assert r == Rusty.echo_term(r)
   end
 end
