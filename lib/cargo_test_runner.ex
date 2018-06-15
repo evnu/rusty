58537988c62eb5cb7ddc96ad573bcbf651bf8079
Magnus Ottenklinger <evnu@posteo.de>
2018-06-16 00:48:09 +0200

    It ain't pretty.. But it'll do...

--- lib/cargo_test_runner.ex
+++ lib/cargo_test_runner.ex
@@ -1,6 +1,8 @@
 defmodule CargoTestRunner do
   use Rustler, otp_app: :rusty, crate: "cargotestrunner"
 
+  # TODO due to recompilation within the NIF, this might take a while
+  @moduletag timeout: (10 * 60 * 1_000)
   def run_tests do
     # TODO this should not be hard-coded. Maybe it can be inserted from the `use` directive
     manifest_path = "/home/evnu/src/elixir/rusty/native/cargotestrunner/Cargo.toml"
--- test/cargo_test_runner_test.exs
+++ test/cargo_test_runner_test.exs
@@ -1,6 +1,8 @@
 defmodule CargoTestRunnerTest do
   use ExUnit.Case
 
+  # TODO due to recompilation within the NIF, this might take a while
+  @tag timeout: (10 * 60 * 1_000)
   test "all" do
     assert :ok == CargoTestRunner.run_tests()
   end
